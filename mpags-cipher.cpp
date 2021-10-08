0b11d8bd4d015dbbadd95797f9e4405ffe8d69a4
Jack Sanders <jack.sanders@cern.ch>
2021-10-08 10:24:36 +0000

    Added operators and char variable

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -5,11 +5,20 @@ int main() {
      Author: Jack Sanders
      File created: 08/10/21 */
     int a{4};
-    const double d{5.5};
+    int b{11};
+    const double d{0.7};
     std::string msg{"a is a int and d is a double"};
-    a = 60;
+    std::string text{"Good Morning"};
+    char fix = text[5];
+    a = 70;
+    std::cout << fix << std::endl;
     std::cout << a << std::endl;
     std::cout << d << std::endl;
+    std::cout << a*d << std::endl;
+    std::cout << d/a << std::endl;
+    std::cout << a/b << std::endl;
+    /* Dividing an int by and int produces an int but 
+    if it produces a decimal value it rounds to a int */
     std::cout << msg << std::endl;
     /* error from adding d=9.0:
     mpags-cipher.cpp: In function ‘int main()’:
