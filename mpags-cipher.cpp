04fb3d81fd4839ec4e37a7975601e034d6b38007
Jack Sanders <jack.sanders@cern.ch>
2021-10-08 09:59:27 +0000

    Added variables to be printed and removed Hello world

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 int main() {
-    /* Hello World test
+    /* Getting Experience
      Author: Jack Sanders
      File created: 08/10/21 */
-     std::cout << "Hello World!\n";
+     int a{4};
+     double d{5.5};
+     a = 90;
+     d = 67.5;
+     std::cout << a << std::endl;
+     std::cout << d << std::endl;
+     /* Doing int e{d}; produces error:
+        warning: narrowing conversion of ‘d’ from ‘double’ to ‘int’ inside { } [-Wnarrowing]
+      int e{d}; */
 }
