eccb06fecf279c0a8b850a64a8b06ca1a46b7b4b
Jack Sanders <jack.sanders@cern.ch>
2021-10-08 14:50:09 +0000

    Added flag funtionality

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -2,13 +2,20 @@
 #include <string>
 #include <vector>
 #include <ctype.h>
+#include<string.h>
 
 int main(int argc, char* argv[]) {
 
     const std::vector<std::string> cmdLineArgs { argv, argv+argc};
     char hflag{0};
+    char iflag{0};
+    char oflag{0};
+    char check_iflag{0};
+    char check_oflag{0};
     std::string i_filename{};
     std::string o_filename{};
+    int i_size{};
+    int o_size{};
     for (int i{0}; i<argc; i++) {
         std::string check{cmdLineArgs[i]};
             if (check == "-h") {
@@ -25,15 +32,35 @@ int main(int argc, char* argv[]) {
                 std::cout << "mpags-cipher version 0.1.0" << std::endl;
             } else if (cmdLineArgs[i-1] == "-i") {
                 i_filename =  check;
+                i_size = i_filename.size();
+                if (i_filename[i_size-1] == 't' && i_filename[i_size-2] == 'x' && i_filename[i_size-3] == 't' && i_filename[i_size-4] == '.' ){
+                    iflag = 1;
+                } else {
+                    check_iflag = 1;
+                    std::cout << "Wrong input file format, needs to be .txt" <<std::endl;
+                }
             } else if (cmdLineArgs[i-1] == "-o") {
                 o_filename =  check;
+                o_size = o_filename.size();
+                if (o_filename[o_size-1] == 't' && o_filename[o_size-2] == 'x' && o_filename[o_size-3] == 't' && o_filename[o_size-4] == '.' ){
+                oflag = 1;
+            } else {
+                check_oflag = 1;
+                std::cout << "Wrong output file format, needs to be .txt" <<std::endl;
             }
+            }
+            
             
         //std::cout << cmdLineArgs[i] << std::endl;
     }
+    if (oflag == 0 && check_oflag == 0) {
+        std::cout << "No declared output file!" << std::endl;
+    } else if (iflag == 0 && check_iflag == 0) {
+        std::cout << "No declared Input file!" << std::endl;
+    }
     // Take each letter from user input:
     char in_char('x');
-    if (hflag == 0) {
+    if (hflag == 0 && iflag ==1 && oflag ==1) {
         std::string u_char{};
     while(std::cin >> in_char) {
         // Take in_char and converts it to upper case
