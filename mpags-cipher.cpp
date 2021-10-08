558e9a438587de453fc220c0a94443ad145ddfc8
Jack Sanders <jack.sanders@cern.ch>
2021-10-08 10:09:49 +0000

    Added string printout

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
+#include <string>
 int main() {
     /* Getting Experience
      Author: Jack Sanders
      File created: 08/10/21 */
     int a{4};
     const double d{5.5};
+    std::string msg{"a is a int and d is a double"};
     a = 60;
     std::cout << a << std::endl;
     std::cout << d << std::endl;
+    std::cout << msg << std::endl;
     /* error from adding d=9.0:
     mpags-cipher.cpp: In function ‘int main()’:
     mpags-cipher.cpp:9:9: error: assignment of read-only variable ‘d’
