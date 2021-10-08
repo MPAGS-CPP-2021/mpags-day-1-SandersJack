b83ef054d26c477ec272f0090bd066b982c03d5e
Jack Sanders <jack.sanders@cern.ch>
2021-10-08 10:07:13 +0000

    Added const double variable and changed value of int a

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -3,14 +3,14 @@ int main() {
     /* Getting Experience
      Author: Jack Sanders
      File created: 08/10/21 */
-     int a{4};
-     double d{5.5};
-     a = 90;
-     d = 67.5;
-     std::cout << a << std::endl;
-     std::cout << d << std::endl;
-     /* Doing int e{d}; produces error:
-        warning: narrowing conversion of ‘d’ from ‘double’ to ‘int’ inside { } [-Wnarrowing]
-      int e{d}; */
+    int a{4};
+    const double d{5.5};
+    a = 60;
+    std::cout << a << std::endl;
+    std::cout << d << std::endl;
+    /* error from adding d=9.0:
+    mpags-cipher.cpp: In function ‘int main()’:
+    mpags-cipher.cpp:9:9: error: assignment of read-only variable ‘d’
+     d = 9.0; */
     return 0;
 }
